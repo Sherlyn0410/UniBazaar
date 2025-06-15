cf6216266e8757f461cdc8a7af7c8f99b9323db5
Sherlyn Kuan <sherlynkuan01@gmail.com>
2025-06-15 23:24:34 +0800

    product upload ui

--- resources/views/product-upload.blade.php
+++ resources/views/product-upload.blade.php
@@ -1,34 +1,38 @@
 <x-app-layout>
-    <div class="container">
-        <form method="post" action="{{route('products.store')}}" class="my-form" enctype="multipart/form-data"
-        >
-            @csrf
-            @method('post')
-            <div class="form-group">
-                <label for="product_name">Product Name</label>
-                <input type="text" name="product_name" id="product_name">
-            </div>
+    <div class="container-fluid bg-white">
+        <div class="container py-4">
+            <div>
+                <h3 class="mb-4 text-left ">Create New Product Listing</h3>
+                <form method="post" action="{{ route('products.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="mb-4">
+                        <label for="product_image" class="form-label fw-semibold">Product Image</label>
+                        <input type="file" name="product_image" id="product_image" class="form-control" accept="image/*" required>
+                    </div>
 
-            <div class="form-group">
-                <label for="product_name">Product Price</label>
-                <input type="text" name="product_price" id="product_price">
-            </div>
+                    <div class="mb-3">
+                        <label for="product_name" class="form-label fw-semibold">Product Name</label>
+                        <input type="text" name="product_name" id="product_name" class="form-control" required>
+                    </div>
 
-            <div class="form-group">
-                <label for="product_name">Product Details</label>
-                <input type="text" name="product_details" id="product_details">
-            </div>
+                    <div class="mb-3">
+                        <label for="product_price" class="form-label fw-semibold">Product Price (RM)</label>
+                        <input type="number" step="0.01" name="product_price" id="product_price" class="form-control" required>
+                    </div>
 
-            <div class="form-group">
-                <label for="product_image">Product Image</label>
-                <input type="file" name="product_image" id="product_image" class="form-control" accept="image/*">
-            </div>
+                    <div class="mb-3">
+                        <label for="product_details" class="form-label fw-semibold">Product Details</label>
+                        <textarea name="product_details" id="product_details" class="form-control" rows="4" required></textarea>
+                    </div>
 
-            <div class="form-group text-center">
-                <button type="submit" class="btn btn-primary">Create Listing</button>
+                    <div class="mt-5 d-block text-end">
+                        <x-red-button type="button">
+                            <i class="bi bi-plus-circle me-2"></i>
+                            {{ __(' Create Listing') }}
+                        </x-red-button>
+                    </div>
+                </form>
             </div>
-        </form>
+        </div>       
     </div>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </x-app-layout>
-</html>
